8a603b431a7fb8a3396e41701861e216e5ee43ef
Lukáš Hubač <hubaclu2@fit.cvut.cz>
2025-06-09 09:55:13 +0200

    Add typing animation.

---
+++ index.js
@@ -0,0 +1,49 @@
+let TYPING_SPEED = [20, 60];
+
+function typeWriterEffect(element) {
+    return new Promise(resolve => {
+        let i = 0;
+        let text = element.innerHTML;
+        element.innerHTML = "";
+        element.style.display = "block";
+        function typeNextLetter() {
+            if (i < text.length) {
+                element.textContent += text.charAt(i);
+                i++;
+
+                // keep reader on the tract of writing
+                if (document.body.offsetHeight - (window.innerHeight + window.scrollY) < 60) {
+                    window.scrollTo(0, document.body.scrollHeight);
+                }
+
+                // simulate the typing with "animation"
+                const randomSpeed = Math.random() * (TYPING_SPEED[1] - TYPING_SPEED[0]) + TYPING_SPEED[0];
+                setTimeout(typeNextLetter, randomSpeed);
+            } else {
+                resolve();
+            }
+        }
+        typeNextLetter();
+    });
+}
+
+async function typeWriterEffectSequential(selector) {
+    const elements = document.querySelectorAll(selector);
+    for (const element of elements) {
+        element.style.display = "none";
+    }
+    for (const element of elements) {
+        await typeWriterEffect(element);
+        await new Promise(resolve => setTimeout(resolve, 10 * TYPING_SPEED[1]));
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    typeWriterEffectSequential("p");
+
+    document.getElementById("skip").addEventListener("click", function () {
+        console.log("clicked");
+        TYPING_SPEED = [5, 10];
+        this.remove();
+    });
+});
